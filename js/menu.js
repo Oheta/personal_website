e01f8f8a2e54a98b5863b2de227162f8bfce59c1
Theau <theau.ebalard@live.fr>
2023-08-29 18:36:29 +0200

    main menu items styling

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-var menu_ref = document.getElementById("eye-menu");
+var menu_ref = document.getElementById("eye-mainMenu");
 console.log(menu_ref);
 
 function placeChildrenRadial(parent_ref, radius, angle_top = 0, angle_bottom = 360)
@@ -7,10 +7,13 @@ function placeChildrenRadial(parent_ref, radius, angle_top = 0, angle_bottom = 3
     for(let i = 0; i < parent_ref.childElementCount; i++)
     {
         let childAngle = i*anglePerChild + angle_top;
-        console.log(childAngle);
-        parent_ref.children[i].style.transform = "rotate(" + childAngle +"deg) + translateX(" + radius +"px)";
-        /* parent_ref.children[i].children[0].style.transform = "rotate(" + (-childAngle) +"deg)"; */
+        //console.log(childAngle);
+        parent_ref.children[i].style.transform = "rotate(" + childAngle +"deg) translateX(" + radius +"px)";
+        //console.log(parent_ref.children[i]);
+        console.log(parent_ref.children[i].style.transform);
+        parent_ref.children[i].children[0].style.transform = "rotate(" + (-childAngle) +"deg)";
+        console.log(parent_ref.children[i].children[0].style.transform);
     }
 }
 
-placeChildrenRadial(menu_ref, 50, 5, -180);
+placeChildrenRadial(menu_ref, 130, -60, 100);
