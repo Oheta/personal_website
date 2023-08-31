b0d22a1d2f1d50ec6c2b5c5ce32a0763c4de3855
Theau <theau.ebalard@live.fr>
2023-08-31 15:20:55 +0200

    main menu editing and styling

--- js/eye_behavior.js
+++ js/eye_behavior.js
@@ -7,9 +7,8 @@ function eyeTrackMouse(){
     cursorevent.forEach((eventType) => {
         document.body.addEventListener(eventType, event => {
             eyeball_ref.forEach((eyeball) => {
-                let eyeballX = eyeball.getBoundingClientRect().left +eyeball.clientWidth /2;
+                let eyeballX = eyeball.getBoundingClientRect().left + eyeball.clientWidth /2;
                 let eyeballY = eyeball.getBoundingClientRect().top + eyeball.clientHeight /2;
-                /*console.log(eyeballX,eyeballY); */
                 var x = !isTouchScreen() ? event.clientX : event.touches[0].clientX;
                 var y = !isTouchScreen() ? event.clientY : event.touches[0].clientY;
                 let radian = Math.atan2(x - eyeballX, y - eyeballY);
--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,30 @@
 var menu_ref = document.getElementById("eye-mainMenu");
-console.log(menu_ref);
 
-function placeChildrenRadial(parent_ref, radius, angle_top = 0, angle_bottom = 360)
+/**
+ * Places children div around a parent div.
+ * @param {HTMLElement} parent_ref Reference to the parent element, around which you are trying to place its children 
+ * @param {Number} radius Distance between children and parent div.
+ * @param {Number} angle_min Minimal angle in degrees. Defaults to 0
+ * @param {Number} angle_max Maximal angle in degrees. Defaults to 360
+ * @param {Boolean} verbose If console output is needed. Defaults to false
+ */
+function placeChildrenRadial(parent_ref, radius, angle_min = 0, angle_max = 360, verbose = false)
 {
-    var anglePerChild = (angle_bottom - angle_top) / parent_ref.childElementCount;
+    var anglePerChild = (angle_max - angle_min) / parent_ref.childElementCount;
     for(let i = 0; i < parent_ref.childElementCount; i++)
     {
-        let childAngle = i*anglePerChild + angle_top;
-        console.log(childAngle);
+        let childAngle = i*anglePerChild + angle_min;
         parent_ref.children[i].style.transform = 
         "translateX(" + radius*Math.cos(degreesToRadians(childAngle)) +"px) " +
         "translateY(" + radius*Math.sin(degreesToRadians(childAngle)) +"px)";
-        //console.log(parent_ref.children[i]);
-        console.log(parent_ref.children[i].style.transform);
-        //parent_ref.children[i].children[0].style.transform = "rotate(" + (-childAngle) +"deg)";
-        console.log(parent_ref.children[i].children[0].style.transform);
+        if(verbose)
+        {
+            console.log("Angle at which child "+ i + " of " + parent_ref +  " is placed : " + childAngle);
+            console.log("placeChildrenRadial() around " + parent_ref + 
+            "Translation x = " + radius*Math.cos(degreesToRadians(childAngle)) + 
+            "Translation y = " + radius*Math.sin(degreesToRadians(childAngle)));
+        }
+
     }
 }
 placeChildrenRadial(menu_ref, 130, -60, 100);
