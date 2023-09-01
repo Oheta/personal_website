e0806f0e9abbea8ad2280104b292f797103bf337
Theau <theau.ebalard@live.fr>
2023-09-01 20:16:57 +0200

    Added main pages and more responsive styling

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ var menu_ref = document.getElementById("eye-mainMenu");
 /**
  * Places children div around a parent div.
  * @param {HTMLElement} parent_ref Reference to the parent element, around which you are trying to place its children 
- * @param {Number} radius Distance between children and parent div.
+ * @param {Number} radius Distance between children and parent element in px.
  * @param {Number} angle_min Minimal angle in degrees. Defaults to 0
  * @param {Number} angle_max Maximal angle in degrees. Defaults to 360
  * @param {Boolean} verbose If console output is needed. Defaults to false
@@ -27,4 +27,49 @@ function placeChildrenRadial(parent_ref, radius, angle_min = 0, angle_max = 360,
 
     }
 }
-placeChildrenRadial(menu_ref, 130, -60, 100);
+/** Shows the element containing [Introduction] */
+function showIntroduction(){
+    let menuItem1_ref = document.getElementById("Introduction");
+    let menuItem2_ref = document.getElementById("Experience");
+    let menuItem3_ref = document.getElementById("Portfolio");
+    let menuItem4_ref = document.getElementById("Contact");
+    menuItem1_ref.style.left = "20vw";
+    menuItem2_ref.style.left = "120vw";
+    menuItem3_ref.style.left = "120vw";
+    menuItem4_ref.style.left = "120vw";
+}
+/** Shows the element containing [Experience] */
+function showExperience(){
+    let menuItem1_ref = document.getElementById("Introduction");
+    let menuItem2_ref = document.getElementById("Experience");
+    let menuItem3_ref = document.getElementById("Portfolio");
+    let menuItem4_ref = document.getElementById("Contact");
+    menuItem1_ref.style.left = "120vw";
+    menuItem2_ref.style.left = "20vw";
+    menuItem3_ref.style.left = "120vw";
+    menuItem4_ref.style.left = "120vw";
+}
+/** Shows the element containing [Portfolio] */
+function showPortfolio(){
+    let menuItem1_ref = document.getElementById("Introduction");
+    let menuItem2_ref = document.getElementById("Experience");
+    let menuItem3_ref = document.getElementById("Portfolio");
+    let menuItem4_ref = document.getElementById("Contact");
+    menuItem1_ref.style.left = "120vw";
+    menuItem2_ref.style.left = "120vw";
+    menuItem3_ref.style.left = "20vw";
+    menuItem4_ref.style.left = "120vw";
+}
+/** Shows the element containing [Contact] */
+function showContact(){
+    let menuItem1_ref = document.getElementById("Introduction");
+    let menuItem2_ref = document.getElementById("Experience");
+    let menuItem3_ref = document.getElementById("Portfolio");
+    let menuItem4_ref = document.getElementById("Contact");
+    menuItem1_ref.style.left = "120vw";
+    menuItem2_ref.style.left = "120vw";
+    menuItem3_ref.style.left = "120vw";
+    menuItem4_ref.style.left = "20vw";
+}
+
+placeChildrenRadial(menu_ref, 100, -60, 100);
